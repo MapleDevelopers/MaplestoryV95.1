94251b13e2479ba7a4b6712b0ad036724b8f500e
Bia10 <t.locke1234@gmail.com>
2021-07-17 22:13:21 +0200

    wip

--- Interfaces/Definition/IUIMsgHandler.h
+++ Interfaces/Definition/IUIMsgHandler.h
@@ -1,7 +1,8 @@
 #pragma once
-#include "../Classes/Definition/ZList.h"
-#include "../Classes/Definition/ZArray.h"
-#include "../Classes/Definition/ZXString.h"
+#include "ZList.h"
+#include "ZArray.h"
+#include "ZXString.h"
+#include "IDraggable.h"
 
 class IUIMsgHandler
 {
@@ -9,20 +10,25 @@ class IUIMsgHandler
 
 public:
 	virtual void OnKey(unsigned int wParam, unsigned int lParam) = 0;
-	virtual int OnSetFocus(int) = 0;
 	virtual void OnMouseButton(unsigned int msg, unsigned int wParam, int rx, int ry) = 0;
-	virtual int OnMouseMove(int rx, int ry) = 0;
-	virtual int OnMouseWheel(int, int, int) = 0;
 	virtual void OnMouseEnter(bool bEnter) = 0;
-	virtual void OnDraggableMove(int, void*, int, int) = 0;
+	virtual void OnDraggableMove(int nState, IDraggable* pObj, int rx, int ry) = 0;
+
+	virtual int OnMouseMove(int rx, int ry) = 0;
+	virtual int OnMouseWheel(int rx, int ry, int nWheel) = 0;
+	virtual int OnSetFocus(int bFocus) = 0;
+
 	virtual void SetEnable(bool bEnable) = 0;
-	virtual bool IsEnabled() = 0;
 	virtual void SetShow(bool bShow) = 0;
+	virtual bool IsEnabled() = 0;
 	virtual bool IsShown() = 0;
+
 	virtual int GetAbsLeft(IUIMsgHandler*) = 0;
-	virtual int* GetAbsTop(IUIMsgHandler*) = 0;
+	virtual int GetAbsTop(IUIMsgHandler*) = 0;
+	virtual CRTTI GetRTTI(IUIMsgHandler*) = &IUIMsgHandler::ms_RTTI_IUIMsgHandler;
+	virtual CRTTI IsKindOf(IUIMsgHandler*, CRTTI* pRTTI);
 	virtual void ClearToolTip(IUIMsgHandler*) = 0;
-	virtual void OnIMEModeChange(IUIMsgHandler*, char) = 0;
-	virtual void OnIMEResult(IUIMsgHandler*, const char*) = 0;
-	virtual void OnIMEComp(IUIMsgHandler*, const char*, ZArray<unsigned long>*, unsigned int, int, ZList<ZXString<char>>*, int, int, int) = 0;
+	virtual void OnIMEModeChange(IUIMsgHandler*, char cMode) = 0;
+	virtual void OnIMEResult(IUIMsgHandler*, const char* sComp) = 0;
+	virtual void OnIMEComp(IUIMsgHandler*, const char* sComp, ZArray<unsigned long>* adwCls, unsigned int nClsIdx, int nCursor, ZList<ZXString<char>>* lCand, int nBegin, int nPage, int nCur) = 0;
 };
